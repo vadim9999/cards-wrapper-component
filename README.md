f073865232144fc15735f1761b56b0e5edc1716a
Arthur Tkachenko <atherdon@gmail.com>
2019-02-03 16:16:59 +0200

    clean up + an explanation

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ Stretch goals:
 - [ ] move components from main recipe-antd repository
 - [ ] import them well and export them
 - [ ] first npm publish/build
-- [ ]
+- [ ] connect/install this package at recipe-antd and maybe at showcase project too.
+- [ ] use components from this package instead of local versions. Recipe-antd should be totally depending on cards from this package
+- [ ] next stage of changes. When Recipe-antd will gain some 'meat' of code - it'll add more requirements/ use cases to this package. it'll help us to identify the problem that can be solved here. Solve that problems and ship another version of this package.
 
 - [ ] create a set of 10-12 tasks related to future releases
 - [ ] move this repository into a next level. figure out how?
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import DownloadPDFLink   from '~/DownloadPDFLink/DownloadPDFLink';
-
-import GeneratePDFButton from '~/GeneratePDFButton/GeneratePDFButton';
-
-import DocumentLayouts    from '~/DocumentLayout/DocumentLayouts';
-
-import RenderLists1       from '~/RenderLists/RenderLists1';
-import RenderLists2       from '~/RenderLists/RenderLists2';
-import RenderLists3       from '~/RenderLists/RenderLists3';
-import RenderLists4       from '~/RenderLists/RenderLists4';
+// import DownloadPDFLink   from '~/DownloadPDFLink/DownloadPDFLink';
+//
+// import GeneratePDFButton from '~/GeneratePDFButton/GeneratePDFButton';
+//
+// import DocumentLayouts    from '~/DocumentLayout/DocumentLayouts';
+//
+// import RenderLists1       from '~/RenderLists/RenderLists1';
+// import RenderLists2       from '~/RenderLists/RenderLists2';
+// import RenderLists3       from '~/RenderLists/RenderLists3';
+// import RenderLists4       from '~/RenderLists/RenderLists4';
 
 export {
-  DownloadPDFLink,
-  GeneratePDFButton,
-  DocumentLayouts,
-
-  RenderLists1,
-  RenderLists2,
-  RenderLists3,
-  RenderLists4
+  // DownloadPDFLink,
+  // GeneratePDFButton,
+  // DocumentLayouts,
+  //
+  // RenderLists1,
+  // RenderLists2,
+  // RenderLists3,
+  // RenderLists4
 }
