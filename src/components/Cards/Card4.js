8ace6f4ec39c6280400123b90644a6b38e72c758
Audax <audax.anchirinah@andela.com>
2019-02-23 03:50:19 +0000

    -Added Card4

---
+++ src/components/Cards/Card4.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import _        from "lodash";
+import { Card, Icon, Avatar, Col, Row } from 'antd';
+
+const { Meta } = Card;
+
+
+
+const Card4 = () => {
+  const styles = {
+    width: 300
+  };
+
+
+
+
+  return (
+    <div style={{ background: '#ECECEC', padding: '30px' }}>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>Card content</Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>Card content</Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>Card content</Card>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Card4
