a2c653b8f5b30e333b70c945e9cbfa90fb022692
Audax <audax.anchirinah@andela.com>
2019-02-23 03:47:22 +0000

    - Added Card3

---
+++ src/components/Cards/Card3.js
@@ -0,0 +1,48 @@
+
+import React, { Component, Fragment } from 'react';
+
+import { Card, Icon, Avatar, Col, Row } from 'antd';
+
+const { Meta } = Card;
+
+
+const Card3 = ({ title, description, children, type = 'all' }) => {
+
+  const styles = {
+    width: 300
+  };
+
+  const cover = <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />
+
+  // actions
+  let actions
+  switch (type) {
+    case 'all':
+      actions = [
+        <Icon type="setting" />,
+        <Icon type="edit" />,
+        <Icon type="ellipsis" />
+      ];
+      break;
+    default:
+      actions = [];
+
+  }
+
+
+  return (
+    <Card
+      style={styles}
+      cover={cover}
+      actions={actions}
+    >
+      <Meta
+        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+        title={title}
+        description={description}
+      />
+    </Card>
+  )
+}
+
+export default Card3
