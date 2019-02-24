19d16480ec6cbe53cc59741cf7844b53835bbc59
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-24 05:47:13 +0200

    possible fix?

--- src/components/ClearCardsExample/ClearCardsExample.js
+++ src/components/ClearCardsExample/ClearCardsExample.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 import { Divider } from 'antd'
 
 import { DefaultCard1, DefaultCard2, DefaultCard3, DefaultCard4, DefaultCard5
-} from './index'
+} from '/index'
 
 import {
   RecipeCard1,
@@ -11,7 +11,7 @@ import {
   RecipeCard3,
   RecipeCard4,
   RecipeCard5,
-} from './index'
+} from '/index'
 
 
 const ClearCardsExample = () => {
